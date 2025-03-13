9277f5a164272f475dca7eed4115057a25ec25ba
Rhed <coolmirth35@gmail.com>
2025-03-13 16:23:12 +0100

    update api

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,3 +1,4 @@
-const String baseUrl = 'http://192.168.1.153:4000/';
+// const String baseUrl = 'http://192.168.1.153:4000/';
 // const String baseUrl = 'http://localhost:4000/';
-// const String baseUrl = 'https://80c180zm-4000.uks1.devtunnels.ms/';
+const String baseUrl =
+    'https://4000-idx-ims-backend-1741875723832.cluster-rz2e7e5f5ff7owzufqhsecxujc.cloudworkstations.dev/';
