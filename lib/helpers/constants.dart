eed6dd774d26527c36293984a41cddedfb42d9bc
Rhed <coolmirth35@gmail.com>
2025-04-30 15:36:03 +0100

    url

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -2,4 +2,6 @@
 // const String baseUrl = 'http://localhost:4000/';
 // const String baseUrl =
 //     'https://4000-idx-ims-backend-1741875723832.cluster-rz2e7e5f5ff7owzufqhsecxujc.cloudworkstations.dev/';
-const String baseUrl = 'https://afraid-lavinie-vessel-5f4a2c7c.koyeb.app/';
+// const String baseUrl = 'https://afraid-lavinie-vessel-5f4a2c7c.koyeb.app/';
+
+const String baseUrl = 'https://3d7c-102-90-102-135.ngrok-free.app/';
